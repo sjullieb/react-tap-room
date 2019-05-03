df9ee7dcad4c2aa2839a8d61157ccb48962092ed
Yulia Shidlovskaya <yulia@>
2019-05-02 17:51:31 -0700

    edit New Keg form

--- src/components/NewKeg.jsx
+++ src/components/NewKeg.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
 
 function NewKeg(props){
+  _name = null;
+  _brand = null;
+  _type = null;
+  _alcohol = null;
+  _price = null;
+
+  onSubmit(){
+    props.onCreateNewKeg({name: _name.value, brand: _brand.value, type: _type.value, alcohol: _alcohol.value, price: _price.value});
+    _name = '';
+    _brand = '';
+    _type = '';
+    _alcohol = '';
+    _price = '';
+  }
+
   return(
   <div>
     <form>
       <label>Name </label>
-      <input id='name' type='text' required></input><br/>
+      <input
+        ref=((input) => {_name = {input};})
+        id='name' type='text' required></input><br/>
       <label>Brand </label>
-      <input id='brand' type='text' required></input><br/>
+      <input
+        ref=((input) => {_brand = {input};})
+        id='brand' type='text' required></input><br/>
       <label>Type </label>
-      <input id='type' type='text' required></input><br/>
+      <input
+        ref=((input) => {_type = {input};})
+        id='type' type='text' required></input><br/>
       <label>Alcohol content </label>
-      <input id='alcohol' type='number' max="100" min="0" required></input><br/>
+      <input
+        ref=((input) => {_alcohol={input};})
+        id='alcohol' type='number' max="100" min="0" required></input><br/>
       <label>Price </label>
-      <input id='price' type='number' required></input><br/>
-      
-      <button>Add</button>
+      <input
+        ref=((input) => {_price={input};})
+        id='price' type='number' required></input><br/>
+
+      <button type='submit'>Add</button>
     </form>
   </div>
   );
