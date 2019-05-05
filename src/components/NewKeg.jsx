5e4b54e457fc071557970c3fc43f14646deb5330
sjull <sjullieb@gmail.com>
2019-05-05 11:57:09 -0700

    redirect to homepage after submitting new keg form

--- src/components/NewKeg.jsx
+++ src/components/NewKeg.jsx
@@ -1,129 +1,143 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropType from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
-function NewKeg(props){
-  let _name = null;
-  let _brand = null;
-  let _type = null;
-  let _alcohol = null;
-  let _pintLeft = null;
-  let _price = null;
+class NewKeg extends Component {
+  constructor(props){
+    super(props);
+    this.state = { toDashboard : false};
+    this._name = null;
+    this._brand = null;
+    this._type = null;
+    this._alcohol = null;
+    this._pintLeft = null;
+    this._price = null;
 
-  function handleNewKegSubmission(event){
+    this.handleNewKegSubmission = this.handleNewKegSubmission.bind(this);
+    this.handleEditKegSubmission = this.handleEditKegSubmission.bind(this);    
+  }
+
+  handleNewKegSubmission(event){
     event.preventDefault();
-    props.onNewKegCreation(makeNewKegObject());
-    clearVarValues();
+    console.log(this);
+    this.props.onNewKegCreation(this.makeNewKegObject());
+    this.clearVarValues();
+    this.setState({toDashboard: true});
   }
 
-  function handleEditKegSubmission(event){
+  handleEditKegSubmission(event){
     event.preventDefault();
-    props.onKegUpdate(props.kegId, makeNewKegObject());
-    clearVarValues();
+    this.props.onKegUpdate(this.props.kegId, this.makeNewKegObject());
+    this.clearVarValues();
   }
 
-  function makeNewKegObject(){
-    console.log({name: _name.value, brand: _brand.value, type: _type.value, alcohol: _alcohol.value, pintLeft: _pintLeft.value, price: _price.value});
-    return {name: _name.value, brand: _brand.value, type: _type.value, alcohol: _alcohol.value, pintLeft: _pintLeft.value, price: _price.value};
+  makeNewKegObject(){
+  //  console.log({name: this._name.value, brand: this._brand.value, type: this._type.value, alcohol: this._alcohol.value, pintLeft: this._pintLeft.value, price: this._price.value});
+    return {name: this._name.value, brand: this._brand.value, type: this._type.value, alcohol: this._alcohol.value, pintLeft: this._pintLeft.value, price: this._price.value};
   }
 
-  function clearVarValues(){
-    _name = '';
-    _brand = '';
-    _type = '';
-    _alcohol = '';
-    _pintLeft = '';
-    _price = '';
+  clearVarValues(){
+    this._name = '';
+    this._brand = '';
+    this._type = '';
+    this._alcohol = '';
+    this._pintLeft = '';
+    this._price = '';
   }
 
-  var mainStyle={
-    backgroundColor: 'lightyellow',
-    padding: '20px',
-    color: 'black',
-    fontSize: '14pt'
-  };
+  render(){
+    var mainStyle={
+      backgroundColor: 'lightyellow',
+      padding: '20px',
+      color: 'black',
+      fontSize: '14pt'
+    };
 
-  var styleInput={
-    width: '100%',
-    height: '24px'
-  };
+    var styleInput={
+      width: '100%',
+      height: '24px'
+    };
 
-  var styleButton={
-    color: 'grey',
-    width: '80px',
-    padding: '5px',
-    fontSize: '11pt',
-    border: '2px solid',
-    borderRadius: '10px',
-    margin: '5px',
-  };
+    var styleButton={
+      color: 'grey',
+      width: '80px',
+      padding: '5px',
+      fontSize: '11pt',
+      border: '2px solid',
+      borderRadius: '10px',
+      margin: '5px',
+    };
 
-  let content = '';
-  if (typeof props.kegId === 'undefined'){
-    content =
-      <div style={mainStyle}>
-        <form onSubmit={handleNewKegSubmission}>
-          <label>Name </label>
-          <input style={styleInput}
-            ref={(input) => {_name = input;}}
-            id='name' type='text' required></input><br/>
-          <label>Brand </label>
-          <input style={styleInput}
-            ref={(input) => {_brand = input;}}
-            id='brand' type='text' required></input><br/>
-          <label>Type </label>
-          <input style={styleInput}
-            ref={(input) => {_type = input;}}
-            id='type' type='text' required></input><br/>
-          <label>Alcohol content </label>
-          <input style={styleInput}
-            ref={(input) => {_alcohol=input;}}
-            id='alcohol' type='number' max="100" min="0" step="0.1" required></input><br/>
-          <input style={styleInput}
-            ref={(input) => {_pintLeft=input;}}
-            id='pintLeft' type='hidden' defaultValue="124" required></input>
-          <label>Price </label>
-          <input style={styleInput}
-            ref={(input) => {_price=input;}}
-            id='price' type='number' step="0.01" min="0" precision="2" required></input><br/>
-          <button type='submit' style={styleButton}>Add</button>
-        </form>
-      </div>
-    ;
-  } else {
-    content =
-      <div style={mainStyle}>
-        <form onSubmit={handleEditKegSubmission}>
-          <label>Name </label>
-          <input style={styleInput}
-            ref={(input) => {_name = input;}}
-            id='name' type='text' defaultValue={props.keg.name} required></input><br/>
-          <label>Brand </label>
-          <input style={styleInput}
-            ref={(input) => {_brand = input;}}
-            id='brand' type='text' defaultValue={props.keg.brand} required></input><br/>
-          <label>Type </label>
-          <input style={styleInput}
-            ref={(input) => {_type = input;}}
-            id='type' type='text' defaultValue={props.keg.type} required></input><br/>
-          <label>Alcohol content </label>
-          <input style={styleInput}
-            ref={(input) => {_alcohol=input;}}
-            id='alcohol' type='number' max="100" min="0" step="0.1" defaultValue={props.keg.alcohol} required></input><br/>
-          <label>Pint left </label>
-          <input style={styleInput}
-            ref={(input) => {_pintLeft=input;}}
-            id='pintLeft' type='number' max="124" min="0" defaultValue={props.keg.pintLeft} required></input><br/>
-          <label>Price </label>
-          <input style={styleInput}
-            ref={(input) => {_price=input;}}
-            id='price' type='number' step="0.01" min="0" precision="2" defaultValue={props.keg.price} required></input><br/>
-          <button type='submit' style={styleButton}>Save</button>
-          <button type='button' style={styleButton} onClick={props.onCancel}>Cancel</button>
-        </form>
-      </div>
-    ;
+    let content = '';
+    if (this.state.toDashboard === true) {
+      content = <Redirect to='/' />;
+    } else if (typeof this.props.kegId === 'undefined'){
+      content =
+        <div style={mainStyle}>
+          <form onSubmit={this.handleNewKegSubmission}>
+            <label>Name </label>
+            <input style={styleInput}
+              ref={(input) => {this._name = input;}}
+              id='name' type='text' required></input><br/>
+            <label>Brand </label>
+            <input style={styleInput}
+              ref={(input) => {this._brand = input;}}
+              id='brand' type='text' required></input><br/>
+            <label>Type </label>
+            <input style={styleInput}
+              ref={(input) => {this._type = input;}}
+              id='type' type='text' required></input><br/>
+            <label>Alcohol content </label>
+            <input style={styleInput}
+              ref={(input) => {this._alcohol=input;}}
+              id='alcohol' type='number' max="100" min="0" step="0.1" required></input><br/>
+            <input style={styleInput}
+              ref={(input) => {this._pintLeft=input;}}
+              id='pintLeft' type='hidden' defaultValue="124" required></input>
+            <label>Price </label>
+            <input style={styleInput}
+              ref={(input) => {this._price=input;}}
+              id='price' type='number' step="0.01" min="0" required></input><br/>
+            <button type='submit' style={styleButton}>Add</button>
+          </form>
+        </div>
+      ;
+    } else {
+      content =
+        <div style={mainStyle}>
+          <form onSubmit={this.handleEditKegSubmission}>
+            <label>Name </label>
+            <input style={styleInput}
+              ref={(input) => {this._name = input;}}
+              id='name' type='text' defaultValue={this.props.keg.name} required></input><br/>
+            <label>Brand </label>
+            <input style={styleInput}
+              ref={(input) => {this._brand = input;}}
+              id='brand' type='text' defaultValue={this.props.keg.brand} required></input><br/>
+            <label>Type </label>
+            <input style={styleInput}
+              ref={(input) => {this._type = input;}}
+              id='type' type='text' defaultValue={this.props.keg.type} required></input><br/>
+            <label>Alcohol content </label>
+            <input style={styleInput}
+              ref={(input) => {this._alcohol=input;}}
+              id='alcohol' type='number' max="100" min="0" step="0.1" defaultValue={this.props.keg.alcohol} required></input><br/>
+            <label>Pint left </label>
+            <input style={styleInput}
+              ref={(input) => {this._pintLeft=input;}}
+              id='pintLeft' type='number' max="124" min="0" defaultValue={this.props.keg.pintLeft} required></input><br/>
+            <label>Price </label>
+            <input style={styleInput}
+              ref={(input) => {this._price=input;}}
+              id='price' type='number' step="0.01" min="0" defaultValue={this.props.keg.price} required></input><br/>
+            <button type='submit' style={styleButton}>Save</button>
+            <button type='button' style={styleButton} onClick={this.props.onCancel}>Cancel</button>
+          </form>
+        </div>
+      ;
+    }
+    return(content);
   }
-  return(content);
 }
 
 NewKeg.propTypes={
